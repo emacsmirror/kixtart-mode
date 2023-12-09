e5e38a7f3f64ff1ba928f497184bda254c971b10
Morgan Willcock <morgan@ice9.digital>
2023-12-09 15:18:55 +0000

    Disable byte compilation of .dir-locals.el
    This adds the standard add-dir-local-variable file header which
    disables byte compilation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((emacs-lisp-mode . ((fill-column . 80)
                      (indent-tabs-mode . nil)))
  (kixtart-mode . ((fill-column . 80)
