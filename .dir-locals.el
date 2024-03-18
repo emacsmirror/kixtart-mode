5b77a94df449272fb54427d4439ba3f82498b7fb
Morgan Willcock <morgan@ice9.digital>
2024-03-18 20:52:17 +0000

    Set fill-column for autoconf-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((change-log-mode . ((mode . outline)
+((autoconf-mode . ((fill-column . 80)))
+ (change-log-mode . ((mode . outline)
                      (fill-column . 70)))
  (emacs-lisp-mode . ((fill-column . 80)
                      (indent-tabs-mode . nil)))
