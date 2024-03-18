d09d825af099808d707936d63d0e61b5c940cba3
Morgan Willcock <morgan@ice9.digital>
2024-03-18 20:48:18 +0000

    Set fill-column for texinfo-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,4 +6,5 @@
  (emacs-lisp-mode . ((fill-column . 80)
                      (indent-tabs-mode . nil)))
  (kixtart-mode . ((fill-column . 80)
-                  (indent-tabs-mode . nil))))
+                  (indent-tabs-mode . nil)))
+ (texinfo-mode . ((fill-column . 70))))
